7b0ebdaa381d782ebbcc5e037a263f9dd6cf45d5
malihatabassum19-1 <malihatabassum19-1@users.noreply.github.com>
2020-11-26 01:25:39 +0600

    Flab2

---
+++ Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flab2
+{
+    class calculator
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Flab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator();
+            Console.WriteLine("Summation : " + c1.Summation(1, 2));
+
+            Console.WriteLine("Subtraction : " + c1.Subtraction(3, 4));
+
+            Console.WriteLine("Multiplication : " + c1.Multiplication(1, 2));
+
+            Console.WriteLine("Division : " + c1.Division(4, 2));
+
+            Scientific c2 = new Scientific();
+           
+            Console.WriteLine("Root : " + c2.Root(4));
+
+            Console.WriteLine("To the power : " + c2.AtoB(2, 3));
+        }
